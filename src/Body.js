081453b5542ddcc2553b9b5ec5ccbf80b7405de3
Ayush (hacktiger) <hacktiger.as@gmail.com>
2022-03-18 13:36:25 +0530

    Hello

--- src/Body.js
+++ src/Body.js
@@ -1,17 +1,18 @@
-import Home from './components/home/Home';
-import "./Style.css"
-import Navbar from './components/navbar/Navbar';
-import About from './components/about/About';
-import Theme from './components/themes/Theme';
-import Team from './components/team/Team';
-import Sponsor from './components/sponsors/Sponsor';
-import Footer from './components/footer/footer';
-import Gallery from './components/gallery/Gallery';
-import Timeline from './components/timeline/Timeline';
-import Faqs from './components/faqs/Faqs';
-import CurrentSponsor from './components/currentSponsers/currentSponsor';
-import Loading from './components/loading/Loading';
-import react,{useState,useEffect} from 'react'
+import Home from "./components/home/Home";
+import "./Style.css";
+import Navbar from "./components/navbar/Navbar";
+import About from "./components/about/About";
+import Theme from "./components/themes/Theme";
+import Team from "./components/team/Team";
+import Sponsor from "./components/sponsors/Sponsor";
+import Footer from "./components/footer/footer";
+import Gallery from "./components/gallery/Gallery";
+import Timeline from "./components/timeline/Timeline";
+import Faqs from "./components/faqs/Faqs";
+import CurrentSponsor from "./components/currentSponsers/currentSponsor";
+import Loading from "./components/loading/Loading";
+import react, { useState, useEffect } from "react";
+import AboutIeee from "./components/about/aboutIeee";
 function Body() {
   const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
@@ -21,23 +22,23 @@ function Body() {
   }, []);
   return (
     <div>
-      {isLoading?
-        <Loading/>:
+      {isLoading ? (
+        <Loading />
+      ) : (
         <div>
-
-    <Navbar/>
-     <Home/>
-     <About/>
-     <Theme/>
-     <CurrentSponsor/>
-     <Sponsor/>
-     <Timeline/>
-     <Gallery/>
-     <Team/>
-     <Footer/>
-     </div>
-    }
-   
+          <Navbar />
+          <Home />
+          <About />
+          <AboutIeee />
+          <Theme />
+          <CurrentSponsor />
+          <Sponsor />
+          <Timeline />
+          <Gallery />
+          <Team />
+          <Footer />
+        </div>
+      )}
     </div>
   );
 }
