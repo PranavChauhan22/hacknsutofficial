49291a74f2a0bed964c0627e2b8037e63e9495c7
utkrsh1203 <utkrsh2212@gmail.com>
2023-01-19 13:01:30 +0530

    SAB DONE

--- src/components/team/Honecombcell.js
+++ src/components/team/Honecombcell.js
@@ -6,13 +6,13 @@ import { BsInstagram } from "react-icons/bs";
 export default function Honecombcell(props) {
   return (
     // <div className={props.className}>
-    <li className={`honeycomb-cell ${props.className}`}>
+    <li className={`honeycomb-cell`}>
       <img className='honeycomb-cell_img' src={props.img} alt='' />
       <div className='honeycomb-cell_title'>{props.title}</div>
-      <a href={props.ln}>
+      <a href={props.ln} className='team-icon'>
         <GrLinkedinOption className='ln_ic' />
       </a>
-      <a href={props.ig}>
+      <a href={props.ig} className='team-icon'>
         <BsInstagram className='ln_ic' />
       </a>
     </li>
